83e674296f0cf957d45df173b998cd759ba2a6fd
Jappie Klooster <jappieklooster@hotmail.com>
2013-11-24 14:02:28 +0100

    added database support

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,38 @@
-class BootStrap {
+import nl.jappieklooster.kook.security.Role
+import nl.jappieklooster.kook.security.User
+import nl.jappieklooster.kook.security.UserRole
 
+class BootStrap {
+    def springSecurityService
     def init = { servletContext ->
+
+		def RoleAdmin = Role.findByAuthority("ROLE_ADMIN") ?: new Role(authority: "ROLE_ADMIN").save()
+		def RoleProducer = Role.findByAuthority("ROLE_PRODUCER") ?: new Role(authority: "ROLE_PRODUCER").save()
+		def RoleCustomer =  Role.findByAuthority("ROLE_CUSTOMER") ?: new Role(authority: "ROLE_CUSTOMER").save()
+
+		def startUsers = [
+			[username:"baal", role: RoleAdmin],
+			[username:"jimmy", role: RoleCustomer],
+			[username:"marvin", role: RoleProducer],
+			[username:"jappie", role: RoleProducer]
+		]
+
+		def users = User.list() ?: []
+
+		if(!users){
+			startUsers.each{ i ->
+				def user = new User(
+					username: i['username'],
+					password: springSecurityService.encodePassword("pass"),
+					enabled: true
+				)
+				if(user.validate()){
+					println "creating user ${i['username']}"
+					user.save(flus:true)
+					UserRole.create user, i['role']
+				}
+			}
+		}
     }
     def destroy = {
     }
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,34 +1,41 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+
+	username = "kook"
+	password = "waerioj2390fs!!jfw3dssWSA"
+
 }
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
-//    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
+    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
 }
 
 // environment specific settings
 environments {
+
+	host = "localhost"
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:mysql://${host}/kook_dev?autoreconnect=true"
+        }
+		hibernate {
+            show_sql = true
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:mysql://${host}/kook_test?useUnicode=yes&characterEncoding=UTF-8"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:mysql://${host}/kook_prod?useUnicode=yes&characterEncoding=UTF-8"
             properties {
                maxActive = -1
                minEvictableIdleTimeMillis=1800000
