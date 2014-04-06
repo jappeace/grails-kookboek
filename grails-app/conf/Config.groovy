834c19079e7199eafe3d6361b8eb3ba1cb583cc0
Jappie Klooster <jappieklooster@hotmail.com>
2014-04-06 15:58:11 +0200

    Changed configuration to allow content negotiation

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -14,9 +14,9 @@
 grails.project.groupId = "nl.jappieklooster.kook" // change this to alter the default package name and Maven publishing destination
 
 // The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
-grails.mime.disable.accept.header.userAgents = ['Gecko', 'WebKit', 'Presto', 'Trident']
+// after a quick review browsers seem to want html again
+//grails.mime.disable.accept.header.userAgents = ['Gecko', 'WebKit', 'Presto', 'Trident']
 grails.mime.types = [
-    all:           '*/*',
     atom:          'application/atom+xml',
     css:           'text/css',
     csv:           'text/csv',
--- web-app/js/ingredients-editor.js
+++ web-app/js/ingredients-editor.js
@@ -1,10 +1,19 @@
 if (typeof jQuery !== 'undefined') {
 	(function($) {
 		$(function() {
-			var urlPrepend = "/kook/";
+			var urlPrepend = "/kook/api/";
+			var units;
+			$.getJSON(
+					urlPrepend+"unit",
+					{},
+					function(result){
+						alert(result);
+						units = JSON.parse(result);
+					}
+				);
+
 			var createRow = function(based){
-			
-			}
+			};
 		});
 	})(jQuery);
 
